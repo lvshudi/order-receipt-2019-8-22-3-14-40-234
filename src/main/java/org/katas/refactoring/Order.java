7f1036418c82e47afdbd0b30aef8fe66fbf6aa76
lvshudi <lvshudi119@126.com>
2019-08-27 20:27:20 +0800

    order's variable

--- src/main/java/org/katas/refactoring/Order.java
+++ src/main/java/org/katas/refactoring/Order.java
@@ -3,25 +3,25 @@ package org.katas.refactoring;
 import java.util.List;
 
 public class Order {
-    String nm;
-    String addr;
-    List<LineItem> li;
+    private String name;
+    private String address;
+    private List<LineItem> itemList;
 
     public Order(String nm, String addr, List<LineItem> li) {
-        this.nm = nm;
-        this.addr = addr;
-        this.li = li;
+        this.name = nm;
+        this.address = addr;
+        this.itemList = li;
     }
 
     public String getCustomerName() {
-        return nm;
+        return name;
     }
 
     public String getCustomerAddress() {
-        return addr;
+        return address;
     }
 
     public List<LineItem> getLineItems() {
-        return li;
+        return itemList;
     }
 }
